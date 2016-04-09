bc378ddd46a5d5013416c07d8831c071b610d6be
Arthur Lui <luiarthur@gmail.com>
2016-04-08 19:26:15 -0700

    yes

---
+++ install_openblas.sh
@@ -0,0 +1,11 @@
+#http://brettklamer.com/diversions/statistical/faster-blas-in-r/
+#http://www.r-bloggers.com/compile-r-and-openblas-from-source-guide/
+#http://www.r-bloggers.com/for-faster-r-use-openblas-instead-better-than-atlas-trivial-to-switch-to-on-ubuntu/
+#http://lists.r-forge.r-project.org/pipermail/rcpp-devel/2014-February/007150.html
+
+# install OpenBLAS
+sudo apt-get install libopenblas-base
+
+# install ATLAS
+sudo apt-get install libatlas3-base liblapack3
+
